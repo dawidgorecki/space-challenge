dc9e2685da114a8fbc966944d19fc269d073d564
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2022-04-01 23:06:18 +0200

    Added SpaceShip interface and Item class

---
+++ src/pl/dawidgorecki/Item.java
@@ -0,0 +1,19 @@
+package pl.dawidgorecki;
+
+public class Item {
+    private String name;
+    private int weight; // in kg
+
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
---
+++ src/pl/dawidgorecki/SpaceShip.java
@@ -0,0 +1,8 @@
+package pl.dawidgorecki;
+
+public interface SpaceShip {
+    public boolean launch();
+    public boolean land();
+    public boolean canCarry(Item item);
+    public void carry(Item item);
+}
